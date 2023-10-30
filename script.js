29424759fcc4ad5852c8922c023a32d6071fd60c
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-10-30 19:49:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-// Your script here.
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
+
+// Populate the list of voices
+function populateVoices() {
+  voices = speechSynthesis.getVoices();
+  voicesDropdown.innerHTML = voices
+    .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+    .join('');
+}
+
+populateVoices();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoices;
+}
+
+// Set the selected voice
+function setVoice() {
+  msg.voice = voices.find(voice => voice.name === voicesDropdown.value);
+}
+
+voicesDropdown.addEventListener('change', setVoice);
+
+// Set rate and pitch
+function setOption() {
+  msg[this.name] = this.value;
+}
+
+options.forEach(option => option.addEventListener('input', setOption
